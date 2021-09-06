e85605bbf13ce65d1e8ec1802c59f8ce3229a7ae
Joel Stevick <jstevickic-1.com@Joels-IC-1-MacBook-Pro.local>
2021-09-06 19:39:51 -0400

    add pom

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-import { mergeConfig as _mergeConfig } from './api/merge-config';
+import { mergeConfig as _mergeConfig } from './api/merge-config/merge-config';
+import { POM as _POM} from './api/pom/pom';
 
+// api
 export const mergeConfig = _mergeConfig;
+
+export const POM = _POM;
