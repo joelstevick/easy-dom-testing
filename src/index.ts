772e0b2e59c9cab37131c9c092bdc14beedb24b6
Joel Stevick <jstevickic-1.com@Joels-IC-1-MacBook-Pro.local>
2021-09-06 20:01:02 -0400

    new api

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-import { mergeConfig as _mergeConfig } from './api/merge-config/merge-config';
-import { POM as _POM } from './api/pom/pom';
 
-// api
-export const mergeConfig = _mergeConfig;
-
-export type POM = _POM;
