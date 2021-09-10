595960783ab6c08896644c9e0c3fd75ca52583e9
Joel Stevick <jstevickic-1.com@Joels-IC-1-MacBook-Pro.local>
2021-09-10 12:46:45 -0400

    no longer validate new states

--- src/api/pom/pom.ts
+++ src/api/pom/pom.ts
@@ -12,23 +12,20 @@ export interface PomConfig {
 export class POM {
   constructor(private context: any, private config: PomConfig) {}
 
-  validate(state: string) {
-    if (!this.config.validate[state]) {
-      throw new Error(`unrecognized validation state: ${state}, check your pom config file validate-property`);
+  validate(key: string) {
+    if (!this.config.validate[key]) {
+      throw new Error(`unrecognized validation: ${key}, check your pom config file validate-property`);
     }
 
-    this.config.validate[state](this.context);
+    this.config.validate[key](this.context);
   }
 
-  action(state: string, ...args: unknown[]) {
-    if (!this.config.actions[state]) {
-      throw new Error(`unrecognized action state: ${state}, check your pom config file actions-property`);
+  action(key: string, ...args: unknown[]) {
+    if (!this.config.actions[key]) {
+      throw new Error(`unrecognized action: ${key}, check your pom config file actions-property`);
     }
 
-    this.config.actions[state].action(this.context, ...args);
+    this.config.actions[key].action(this.context, ...args);
 
-    if (this.config.actions[state]['state-transition']) {
-      this.validate(this.config.actions[state]['state-transition']!);
-    }
   }
 }
